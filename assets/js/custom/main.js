9d13c267f262fb13e325cb63a5830175fee7dd04
Hazra-Aufait <hazra@aufaitux.com>
2024-08-01 09:16:31 +0530

    feat: hp

---
+++ assets/js/custom/main.js
@@ -0,0 +1,37 @@
+function toggleTheme() {
+  const htmlElement = document.documentElement;
+  const currentTheme = htmlElement.getAttribute('data-theme');
+  const lightThemeLink = document.getElementById('light-theme');
+  const darkThemeLink = document.getElementById('dark-theme');
+
+  if (currentTheme === 'theme-light') {
+    htmlElement.setAttribute('data-theme', 'theme-dark');
+    lightThemeLink.disabled = true;
+    darkThemeLink.disabled = false;
+  } else {
+    htmlElement.setAttribute('data-theme', 'theme-light');
+    lightThemeLink.disabled = false;
+    darkThemeLink.disabled = true;
+  }
+
+  updateLogo();
+}
+
+function updateLogo() {
+  const hpLogo = document.getElementById('hp-logo');
+  const hpLogoResp = document.getElementById('hp-logo-resp');
+  const theme = document.documentElement.getAttribute('data-theme');
+
+  if (theme === 'theme-light') {
+    hpLogo.src = './assets/images/svg/SVG/Asset 2.svg';
+    hpLogoResp.src = './assets/images/svg/SVG/Asset 2.svg';
+  } else if (theme === 'theme-dark') {
+    hpLogo.src = './assets/images/HP-Imagine-Logo-Official-RGB-04.svg';
+    hpLogoResp.src = './assets/images/HP-Imagine-Logo-Official-RGB-04.svg';
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Initial call to set the correct logo based on the current theme
+  updateLogo();
+});
